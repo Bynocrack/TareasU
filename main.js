f594cefcd3c3644dbb00300c32242433bc90c8c5
Josue Madueño <josue4500@outlook.es>
2025-11-24 16:35:02 -0500

    ejericios hasta el 6 terminados

--- main.js
+++ main.js
@@ -15,5 +15,40 @@ const ex03 = () => {
 };
 
 const ex04 = () => {
-  console.log("hola");
+  try {
+    let texto = "{nombre: Juan}";
+    let obj = JSON.parse(texto);
+    console.log(obj);
+  } catch (e) {
+    console.log(`Error\n${e.name}\n${e.message}`);
+  }
+};
+
+const ex05 = () => {
+  try {
+    let texto = "hola";
+    console.log(hola.hola);
+  } catch (e) {
+    console.log("falló :C");
+  } finally {
+    console.log("Siempre se imprime.");
+  }
+};
+
+const ex06 = () => {
+  const validarEdad = (edad) => {
+    if (isNaN(edad)) {
+      throw new Error("Edad inválida");
+    }
+    if (edad < 0) {
+      throw new Error("Edad inválida");
+    }
+  };
+
+  try {
+    validarEdad("abv");
+    console.log("todo bien");
+  } catch (e) {
+    console.log(e.message);
+  }
 };
